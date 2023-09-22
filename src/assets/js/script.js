e3c07f0b89019430d21cfe00d22813c098355baa
eapil16 <77381727+eapil16@users.noreply.github.com>
2023-09-22 10:42:33 +0300

    add pages

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -52,4 +52,40 @@ document.addEventListener('DOMContentLoaded', () => {
           swiper: swiper3,
         },
     });
+
+  // 19 09
+    (function($){
+      var sections = [];
+      var id = false;
+      var $navbara = $('#navbar-example a');
+
+      $navbara.click(function(e){
+        e.preventDefault();
+        $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top
+        },50);
+      });
+
+      $navbara.each(function(){
+        sections.push($($(this).attr('href')));
+      })
+
+      $(window).scroll(function(e){
+        var scrollTop = $(this).scrollTop() + ($(window).height()/2);
+        for (var i in sections) {
+          var section = sections[i];
+          if (scrollTop > section.offset().top){
+            var scrolled_id = section.attr('id');
+          }
+        }
+        if (scrolled_id !== id) {
+          id = scrolled_id;
+          $($navbara).removeClass('current');
+          $('#navbar-example a[href="#' + id + '"]').addClass('current');
+        }
+      })
+    })(jQuery);
+
+  // end 19 09
+
 });
